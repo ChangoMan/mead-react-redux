32722b2d790954e0ffb3380170922d5b50654eb9
Hunter Chang <hunter@madwiremedia.com>
2017-05-01 15:27:29 -0600

    add support for location fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
 var redux = require('redux');
+var axios = require('axios');
 
 console.log('Starting redux example');
 
@@ -88,10 +89,58 @@ var removeMovie = (id) => {
     }
 }
 
+// Map reducer and action generators
+// ---------
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+    switch (action.type) {
+        case 'START_LOCATION_FETCH':
+            return {
+                isFetching: true,
+                url: undefined
+            }
+        case 'COMPLETE_LOCATION_FETCH':
+            return {
+                isFetching: false,
+                url: action.url
+            }
+        default:
+            return state;
+    }
+}
+
+var startLocationFetch = () => {
+    return {
+        type: 'START_LOCATION_FETCH'
+    }
+}
+
+var completeLocationFetch = (url) => {
+    return {
+        type: 'COMPLETE_LOCATION_FETCH',
+        url: url
+    }
+}
+
+var fetchLocation = () => {
+    store.dispatch(startLocationFetch());
+    axios.get('http://ipinfo.io').then(function(res) {
+
+        var loc = res.data.loc;
+
+        var baseUrl = 'http://maps.google.com?q='
+
+        store.dispatch(completeLocationFetch(baseUrl+loc));
+
+    }, function(err) {
+        console.log(err)
+    });
+}
+
 var reducer = redux.combineReducers({
     name: nameReducer,
     hobbies: hobbiesReducer,
-    movies: moviesReducer
+    movies: moviesReducer,
+    map : mapReducer
 });
 
 var store = redux.createStore(reducer);
@@ -99,11 +148,18 @@ var store = redux.createStore(reducer);
 // Subscribe to changes
 store.subscribe(() => {
     var state = store.getState();
-    console.log('New State', state)
+    console.log('New State', state);
+    if (state.map.isFetching) {
+        document.getElementById('app').innerHTML = 'Loading...';
+    } else if (state.map.url) {
+        document.getElementById('app').innerHTML = '<a target="_blank" href="'+state.map.url+'">View Your Location</a>'
+    }
 });
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+fetchLocation();
 
 store.dispatch(changeName('Hunter'));
 
